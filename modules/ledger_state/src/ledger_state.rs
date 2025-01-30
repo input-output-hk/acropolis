e58a6547081320f7e3251149ae42158551f1ff8f
Paul Clark <paul.clark@iohk.io>
2025-01-30 11:48:24 +0000

    In-memory ledger state store

--- modules/ledger_state/src/ledger_state.rs
+++ modules/ledger_state/src/ledger_state.rs
@@ -3,13 +3,66 @@
 
 use caryatid_sdk::{Context, Module, module, MessageBusExt};
 use acropolis_messages::Message;
-use std::sync::Arc;
 use anyhow::Result;
 use config::Config;
 use tracing::{info, error};
 use hex::encode;
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
+use std::sync::{Arc, RwLock};
 
 const DEFAULT_SUBSCRIBE_TOPIC: &str = "cardano.utxo.#";
+
+/// Key of ledger state store
+#[derive(Debug, Clone, Eq)]
+struct UTXOKey {
+    hash: [u8; 32], // Tx hash
+    index: u32,     // Output index in the transaction
+}
+
+impl UTXOKey {
+    /// Creates a new UTXOKey from any slice (pads with zeros if < 32 bytes)
+    pub fn new(hash_slice: &[u8], index: u32) -> Self {
+        let mut hash = [0u8; 32]; // Initialize with zeros
+        let len = hash_slice.len().min(32); // Cap at 32 bytes
+        hash[..len].copy_from_slice(&hash_slice[..len]); // Copy input hash
+        Self { hash, index }
+    }
+}
+
+impl PartialEq for UTXOKey {
+    fn eq(&self, other: &Self) -> bool {
+        self.index == other.index && self.hash == other.hash
+    }
+}
+
+impl Hash for UTXOKey {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.hash.hash(state);
+        self.index.hash(state);
+    }
+}
+
+/// Value stored in UTXO
+#[derive(Debug, Clone)]
+struct UTXOValue {
+    address: Vec<u8>, // Address
+    value: u64,       // Value in Lovelace
+}
+
+/// Ledger state storage
+struct State {
+    utxos: HashMap<UTXOKey, UTXOValue>,
+}
+
+impl State {
+    /// Create a new empty state
+    pub fn new() -> Self {
+        Self {
+            utxos: HashMap::new(),
+        }
+    }
+}
 
 /// Ledger state module
 #[module(
@@ -24,6 +77,8 @@ impl LedgerState
     /// Main init function
     pub fn init(&self, context: Arc<Context<Message>>, config: Arc<Config>) -> Result<()> {
 
+        let state = Arc::new(RwLock::new(State::new()));
+
         // Subscribe for UTXO messages
         // Get configuration
         let subscribe_topic = config.get_string("subscribe-topic")
@@ -34,11 +89,23 @@ impl LedgerState
                                       move |message: Arc<Message>| {
            match message.as_ref() {
                Message::Input(input_msg) => {
-                   info!("Received input {}:{}", encode(&input_msg.ref_hash), input_msg.ref_index);
+                   info!("UTXO << {}:{}", encode(&input_msg.ref_hash), input_msg.ref_index);
+                   let key = UTXOKey::new(&input_msg.ref_hash, input_msg.index);
+                   let mut state = state.write().unwrap();
+                   match state.utxos.remove(&key) {
+                       Some(previous) => info!("        - spent {} from {}", previous.value,
+                                               encode(previous.address)),
+                       None => info!("        - not previously seen")
+                   }
                }
 
                Message::Output(output_msg) => {
-                   info!("Received output {}:{}", encode(&output_msg.tx_hash), output_msg.index);
+                   info!("UTXO >> {}:{}", encode(&output_msg.tx_hash), output_msg.index);
+                   info!("        - adding {} to {}", output_msg.value, encode(&output_msg.address));
+                   let key = UTXOKey::new(&output_msg.tx_hash, output_msg.index);
+                   let mut state = state.write().unwrap();
+                   state.utxos.insert(key, UTXOValue { address: output_msg.address.clone(),
+                                                       value: output_msg.value });
                }
 
                _ => error!("Unexpected message type: {message:?}")
