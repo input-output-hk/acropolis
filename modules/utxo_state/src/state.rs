0533fed58a37dbd57aed93b26ec038f53e95efa8
Paul Clark <paul.clark@iohk.io>
2025-03-06 17:33:40 +0000

    Refactor out State in UTXOState

---
+++ modules/utxo_state/src/state.rs
@@ -0,0 +1,142 @@
+//! Acropolis UTXOState: State storage
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
+use acropolis_messages::{BlockInfo, TxInput, TxOutput};
+use tracing::{debug, info, error};
+use hex::encode;
+
+/// Key of ledger state store
+#[derive(Debug, Clone, Eq)]
+pub struct UTXOKey {
+    hash: [u8; 32], // Tx hash
+    index: u64,     // Output index in the transaction
+}
+
+impl UTXOKey {
+    /// Creates a new UTXOKey from any slice (pads with zeros if < 32 bytes)
+    pub fn new(hash_slice: &[u8], index: u64) -> Self {
+        let mut hash = [0u8; 32]; // Initialize with zeros
+        let len = hash_slice.len().min(32); // Cap at 32 bytes
+        hash[..len].copy_from_slice(&hash_slice[..len]); // Copy input hash
+        Self { hash, index }
+    }
+}
+
+impl PartialEq for UTXOKey {
+    fn eq(&self, other: &Self) -> bool {
+        self.index == other.index && self.hash == other.hash
+    }
+}
+
+impl Hash for UTXOKey {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.hash.hash(state);
+        self.index.hash(state);
+    }
+}
+
+/// Value stored in UTXO
+#[derive(Debug, Clone)]
+pub struct UTXOValue {
+    address: Vec<u8>, // Address
+    value: u64,       // Value in Lovelace
+}
+
+/// Ledger state storage
+pub struct State {
+    utxos: HashMap<UTXOKey, UTXOValue>,    //< Live UTXOs
+    future_spends: HashMap<UTXOKey, u64>,  //< UTXOs spent in blocks arriving out of order, to slot
+    max_slot: u64,                         //< Maximum block slot received
+    max_number: u64,                       //< Maximum block number received
+}
+
+impl State {
+    /// Create a new empty state
+    pub fn new() -> Self {
+        Self {
+            utxos: HashMap::new(),
+            future_spends: HashMap::new(),
+            max_slot: 0,
+            max_number: 0,
+        }
+    }
+
+    /// Notice a block for statistics
+    pub fn notice_block(&mut self, block: &BlockInfo) {
+        self.max_slot = self.max_slot.max(block.slot);
+        self.max_number = self.max_number.max(block.number);
+    }
+
+    /// Notice an input UTXO spend
+    pub fn notice_input(&mut self, input: &TxInput, slot: u64) {
+        let key = UTXOKey::new(&input.tx_hash, input.index);
+        
+        if tracing::enabled!(tracing::Level::DEBUG) {
+            debug!("UTXO << {}:{}", encode(&key.hash), key.index);
+        }
+
+        match self.utxos.remove(&key) {
+            Some(previous) => {
+                if tracing::enabled!(tracing::Level::DEBUG) {
+                    debug!("        - spent {} from {}",
+                           previous.value, encode(previous.address));
+                }
+            }
+            None => {
+                if tracing::enabled!(tracing::Level::DEBUG) {
+                    debug!("UTXO {}:{} arrived out of order (slot {})",
+                           encode(&key.hash), key.index, slot);
+                }
+                // Add to future spend set
+                self.future_spends.insert(key, slot);
+            }
+        }
+    } 
+
+    /// Notice an output UXTO creation
+    pub fn notice_output(&mut self,  output: &TxOutput, slot: u64) {
+
+        let key = UTXOKey::new(&output.tx_hash, output.index);
+
+        if tracing::enabled!(tracing::Level::DEBUG) {
+            debug!("UTXO >> {}:{}", encode(&key.hash), key.index);
+            debug!("        - adding {} to {}", output.value, encode(&output.address));
+        }
+
+        // Check if it was spent in a future block (that arrived
+        // out of order)
+        if let Some(old_slot) = self.future_spends.get(&key) {
+            // Net effect is zero, so we ignore it
+            if tracing::enabled!(tracing::Level::DEBUG) {
+                debug!("UTXO {}:{} in future spends removed (created in slot {}, spent in slot {})",
+                        encode(&key.hash), key.index, slot, old_slot);
+            }
+            self.future_spends.remove(&key);
+        } else {
+            self.utxos.insert(key, UTXOValue {
+                address: output.address.clone(),
+                value: output.value
+            });
+        }
+    }
+
+    /// Log statistics
+    pub fn log_stats(&self) {
+        info!(slot = self.max_slot,
+            number = self.max_number,
+            utxos = self.utxos.len(),
+            future_spends = self.future_spends.len());
+      for (key, slot) in &self.future_spends {
+          if tracing::enabled!(tracing::Level::DEBUG) {
+              debug!("Future spend: UTXO {}:{} from slot {slot}",
+                  encode(key.hash), key.index);
+          }
+          if self.max_slot - slot > 10000 {
+              error!("Future spend UTXO {}:{} from slot {slot} is too old (max slot {})",
+                     encode(key.hash), key.index, self.max_slot);
+          }
+      }
+    }
+
+}
+
--- modules/utxo_state/src/utxo_state.rs
+++ modules/utxo_state/src/utxo_state.rs
@@ -6,69 +6,12 @@ use acropolis_messages::{BlockStatus, Message, UTXODelta};
 use anyhow::Result;
 use config::Config;
 use tracing::{debug, info, error};
-use hex::encode;
-use std::collections::HashMap;
-use std::hash::{Hash, Hasher};
 use std::sync::{Arc, RwLock};
 
+mod state;
+use state::State;
+
 const DEFAULT_SUBSCRIBE_TOPIC: &str = "cardano.utxo.deltas";
-
-/// Key of ledger state store
-#[derive(Debug, Clone, Eq)]
-struct UTXOKey {
-    hash: [u8; 32], // Tx hash
-    index: u64,     // Output index in the transaction
-}
-
-impl UTXOKey {
-    /// Creates a new UTXOKey from any slice (pads with zeros if < 32 bytes)
-    pub fn new(hash_slice: &[u8], index: u64) -> Self {
-        let mut hash = [0u8; 32]; // Initialize with zeros
-        let len = hash_slice.len().min(32); // Cap at 32 bytes
-        hash[..len].copy_from_slice(&hash_slice[..len]); // Copy input hash
-        Self { hash, index }
-    }
-}
-
-impl PartialEq for UTXOKey {
-    fn eq(&self, other: &Self) -> bool {
-        self.index == other.index && self.hash == other.hash
-    }
-}
-
-impl Hash for UTXOKey {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.hash.hash(state);
-        self.index.hash(state);
-    }
-}
-
-/// Value stored in UTXO
-#[derive(Debug, Clone)]
-struct UTXOValue {
-    address: Vec<u8>, // Address
-    value: u64,       // Value in Lovelace
-}
-
-/// Ledger state storage
-struct State {
-    utxos: HashMap<UTXOKey, UTXOValue>,    //< Live UTXOs
-    future_spends: HashMap<UTXOKey, u64>,  //< UTXOs spent in blocks arriving out of order, to slot
-    max_slot: u64,                         //< Maximum block slot received
-    max_number: u64,                       //< Maximum block number received
-}
-
-impl State {
-    /// Create a new empty state
-    pub fn new() -> Self {
-        Self {
-            utxos: HashMap::new(),
-            future_spends: HashMap::new(),
-            max_slot: 0,
-            max_number: 0,
-        }
-    }
-}
 
 /// UTXO state module
 #[module(
@@ -104,8 +47,7 @@ impl UTXOState
 
                         { // Capture maximum slot received
                             let mut state = state.write().unwrap();
-                            state.max_slot = state.max_slot.max(deltas_msg.block.slot);
-                            state.max_number = state.max_number.max(deltas_msg.block.number);
+                            state.notice_block(&deltas_msg.block);
                         }
 
                         if let BlockStatus::RolledBack = deltas_msg.block.status {
@@ -115,58 +57,15 @@ impl UTXOState
                         }
 
                         for delta in &deltas_msg.deltas {  // UTXODelta
+                            let mut state = state.write().unwrap();
+
                             match delta {
                                 UTXODelta::Input(tx_input) => {
-                                    if tracing::enabled!(tracing::Level::DEBUG) {
-                                        debug!("UTXO << {}:{}", encode(&tx_input.tx_hash),
-                                              tx_input.index);
-                                    }
-                                    let key = UTXOKey::new(&tx_input.tx_hash, tx_input.index);
-                                    let mut state = state.write().unwrap();
-                                    match state.utxos.remove(&key) {
-                                        Some(previous) => {
-                                            if tracing::enabled!(tracing::Level::DEBUG) {
-                                                debug!("        - spent {} from {}",
-                                                       previous.value, encode(previous.address));
-                                            }
-                                        }
-                                        None => {
-                                            if tracing::enabled!(tracing::Level::DEBUG) {
-                                                debug!("UTXO {}:{} arrived out of order (slot {})",
-                                                    encode(&tx_input.tx_hash), tx_input.index,
-                                                    deltas_msg.block.slot);
-                                            }
-                                            // Add to future spend set
-                                            state.future_spends.insert(key, deltas_msg.block.slot);
-                                        }
-                                    }
+                                    state.notice_input(tx_input, deltas_msg.block.slot);
                                 },
 
                                 UTXODelta::Output(tx_output) => {
-                                    if tracing::enabled!(tracing::Level::DEBUG) {
-                                        debug!("UTXO >> {}:{}", encode(&tx_output.tx_hash),
-                                               tx_output.index);
-                                        debug!("        - adding {} to {}", tx_output.value,
-                                            encode(&tx_output.address));
-                                    }
-                                    let key = UTXOKey::new(&tx_output.tx_hash, tx_output.index);
-                                    let mut state = state.write().unwrap();
-                                    // Check if it was spent in a future block (that arrived
-                                    // out of order)
-                                    if let Some(slot) = state.future_spends.get(&key) {
-                                        // Net effect is zero, so we ignore it
-                                        if tracing::enabled!(tracing::Level::DEBUG) {
-                                            debug!("UTXO {}:{} in future spends removed (created in slot {}, spent in slot {})",
-                                                  encode(&tx_output.tx_hash), tx_output.index,
-                                                  deltas_msg.block.slot, slot);
-                                        }
-                                        state.future_spends.remove(&key);
-                                    } else {
-                                        state.utxos.insert(key, UTXOValue {
-                                            address: tx_output.address.clone(),
-                                            value: tx_output.value
-                                        });
-                                    }
+                                    state.notice_output(&tx_output, deltas_msg.block.slot);
                                 },
 
                                 _ => {}
@@ -183,21 +82,8 @@ impl UTXOState
         context.clone().message_bus.subscribe("clock.tick", move |message: Arc<Message>| {
             if let Message::Clock(message) = message.as_ref() {
                 if (message.number % 60) == 0 {
-                    let state = state2.write().unwrap();
-                    info!(slot = state.max_slot,
-                          number = state.max_number,
-                          utxos = state.utxos.len(),
-                          future_spends = state.future_spends.len());
-                    for (key, slot) in &state.future_spends {
-                        if tracing::enabled!(tracing::Level::DEBUG) {
-                            debug!("Future spend: UTXO {}:{} from slot {slot}",
-                                encode(key.hash), key.index);
-                        }
-                        if state.max_slot - slot > 10000 {
-                            error!("Future spend UTXO {}:{} from slot {slot} is too old (max slot {})",
-                                   encode(key.hash), key.index, state.max_slot);
-                        }
-                    }
+                    let state = state2.read().unwrap();
+                    state.log_stats();
                 }
             }
 
