8a60cc165aec92beaae836846b8a7a44a8ac40a1
Alex Woods <alexander.j.woods@gmail.com>
2025-03-28 13:44:51 +0000

    Add serialisation and retirement to SPO state

---
+++ modules/spo_state/src/state.rs
@@ -0,0 +1,57 @@
+//! Acropolis SPOState: State storage
+use acropolis_common::{
+    messages::TxCertificatesMessage,
+    PoolRegistration,
+    SerialisedMessageHandler,
+    TxCertificate,
+};
+use anyhow::Result;
+use async_trait::async_trait;
+use serde::{Serialize, Serializer};
+use std::collections::HashMap;
+use tracing::info;
+
+pub struct State {
+    spos: HashMap<Vec::<u8>, PoolRegistration>,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            spos: HashMap::<Vec::<u8>, PoolRegistration>::new(),
+        }
+    }
+}
+
+#[async_trait]
+impl SerialisedMessageHandler<TxCertificatesMessage> for State {
+    async fn handle(&mut self, tx_cert_msg: &TxCertificatesMessage) -> Result<()> {
+        for tx_cert in tx_cert_msg.certificates.iter() {
+            match tx_cert {
+                TxCertificate::PoolRegistration(reg) => {
+                    self.spos.insert(reg.operator.clone(), reg.clone());
+                }
+                TxCertificate::PoolRetirement(reg) => {
+                    self.spos.remove(&reg.operator);
+                }
+                _ => ()
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl Serialize for State {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let serialized_map: HashMap<String, PoolRegistration> = self
+            .spos
+            .iter()
+            .map(|(key, value)| (hex::encode(key), value.clone()))
+            .collect();
+        serialized_map.serialize(serializer)
+    }
+}
