a836a208b936dc8198ce09b93acd340e634d4ba8
Paul Clark <paul.clark@iohk.io>
2025-04-01 15:44:30 +0100

    Move test delay to fake store

--- modules/utxo_state/src/in_memory_immutable_utxo_store.rs
+++ modules/utxo_state/src/in_memory_immutable_utxo_store.rs
@@ -8,25 +8,18 @@ use anyhow::Result;
 use config::Config;
 use std::sync::Arc;
 use tracing::info;
-use tokio::time::{sleep, Duration};
 
 pub struct InMemoryImmutableUTXOStore {
     /// Map of UTXOs
     utxos: RwLock<HashMap<UTXOKey, UTXOValue>>,
-
-    /// Delay time for testing
-    delay_us: u64,
 }
 
 impl InMemoryImmutableUTXOStore {
-    pub fn new(config: Arc<Config>) -> Self {
+    pub fn new(_config: Arc<Config>) -> Self {
         info!("Storing immutable UTXOs in memory (standard)");
-
-        let delay_us = config.get_int("delay").unwrap_or(0) as u64;
 
         Self {
             utxos: RwLock::new(HashMap::new()),
-            delay_us,
         }
     }
 }
@@ -37,22 +30,12 @@ impl ImmutableUTXOStore for InMemoryImmutableUTXOStore {
     /// Add a UTXO
     async fn add_utxo(&self, key: UTXOKey, value: UTXOValue) -> Result<()> {
         self.utxos.write().await.insert(key, value);
-
-        if self.delay_us != 0 {
-            sleep(Duration::from_micros(self.delay_us)).await;
-        }
-
         Ok(())
     }
 
     /// Delete a UTXO
     async fn delete_utxo(&self, key: &UTXOKey) -> Result<()> {
         self.utxos.write().await.remove(key);
-
-        if self.delay_us != 0 {
-            sleep(Duration::from_micros(self.delay_us)).await;
-        }
-
         Ok(())
     }
 
