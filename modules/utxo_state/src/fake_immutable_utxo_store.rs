f1f3e3caf7c27cbc37a9637071deab74d22eb432
Paul Clark <paul.clark@iohk.io>
2025-04-01 15:44:30 +0100

    Move test delay to fake store

--- modules/utxo_state/src/fake_immutable_utxo_store.rs
+++ modules/utxo_state/src/fake_immutable_utxo_store.rs
@@ -6,15 +6,26 @@ use async_trait::async_trait;
 use anyhow::Result;
 use config::Config;
 use std::sync::Arc;
-use tracing::error;
+use tracing::{error, info};
+use tokio::time::{sleep, Duration};
 
 pub struct FakeImmutableUTXOStore {
+    /// Delay time for testing
+    delay_us: u64,
 }
 
 impl FakeImmutableUTXOStore {
-    pub fn new(_config: Arc<Config>) -> Self { 
+    pub fn new(config: Arc<Config>) -> Self { 
         error!("Using fake immutable UTXO store!");
-        Self { }
+
+        let delay_us = config.get_int("delay").unwrap_or(0) as u64;
+        if delay_us != 0 {
+            info!("Delay of {delay_us}us on each write");
+        }
+
+        Self {
+            delay_us,
+        }
     }
 }
 
@@ -23,11 +34,19 @@ impl ImmutableUTXOStore for FakeImmutableUTXOStore {
 
     /// Add a UTXO
     async fn add_utxo(&self, _key: UTXOKey, _value: UTXOValue) -> Result<()> {
+        if self.delay_us != 0 {
+            sleep(Duration::from_micros(self.delay_us)).await;
+        }
+
         Ok(())
     }
 
     /// Delete a UTXO
     async fn delete_utxo(&self, _key: &UTXOKey) -> Result<()> {
+        if self.delay_us != 0 {
+            sleep(Duration::from_micros(self.delay_us)).await;
+        }
+
         Ok(())
     }
 
