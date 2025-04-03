f0ad3496d83cc1e9c425243af06fe367f6bcc53f
Paul Clark <paul.clark@iohk.io>
2025-04-03 17:29:15 +0100

    REST endpoint for /utxo (WIP) #19
    Still  needs
    - Parsing of UTXO hash:n format
    - Output of address as standard textual format

--- modules/utxo_state/src/utxo_state.rs
+++ modules/utxo_state/src/utxo_state.rs
@@ -4,7 +4,7 @@
 use caryatid_sdk::{Context, Module, module, MessageBusExt};
 use acropolis_common::{
     Serialiser,
-    messages::Message
+    messages::{Message, RESTResponse},
 };
 
 use anyhow::{Result, anyhow};
@@ -35,6 +35,8 @@ mod fake_immutable_utxo_store;
 use fake_immutable_utxo_store::FakeImmutableUTXOStore;
 
 const DEFAULT_SUBSCRIBE_TOPIC: &str = "cardano.utxo.deltas";
+const DEFAULT_REST_TOPIC: &str = "rest.get.utxo.*";
+
 const DEFAULT_STORE: &str = "memory";
 
 /// UTXO state module
@@ -54,6 +56,10 @@ impl UTXOState
         let subscribe_topic = config.get_string("subscribe-topic")
             .unwrap_or(DEFAULT_SUBSCRIBE_TOPIC.to_string());
         info!("Creating subscriber on '{subscribe_topic}'");
+
+        let rest_topic = config.get_string("rest-topic")
+            .unwrap_or(DEFAULT_REST_TOPIC.to_string());
+        info!("Creating REST handler on '{rest_topic}'");
 
         // Create store
         let store_type = config.get_string("store").unwrap_or(DEFAULT_STORE.to_string());
@@ -89,6 +95,7 @@ impl UTXOState
 
         let state = Arc::new(Mutex::new(state));
         let state2 = state.clone();
+        let state3 = state.clone();
         let serialiser = Arc::new(Mutex::new(Serialiser::new(state, module_path!(), 0)));
         let serialiser2 = serialiser.clone();
 
@@ -127,6 +134,45 @@ impl UTXOState
                 }
             }
         })?;
+
+        // Handle REST requests for utxo.<id>
+        context.message_bus.handle(&rest_topic, move |message: Arc<Message>| {
+            let state = state3.clone();
+
+            async move {
+                let response = match message.as_ref() {
+                    Message::RESTRequest(request) => {
+                        info!("REST received {} {}", request.method, request.path);
+                        match request.path_elements.get(1) {
+                            Some(id) => match hex::decode(&id) {
+                                Ok(id) => {
+                                    let key = state::UTXOKey::new(&id, 0); // TODO parse :index
+                                    match state.lock().await.lookup_utxo(&key).await {
+                                        Ok(Some(utxo)) => match serde_json::to_string(&utxo) {
+                                            Ok(body) => RESTResponse::with_json(200, &body),
+                                            Err(error) => RESTResponse::with_text(500, 
+                                                &format!("{error:?}").to_string()),
+                                        },
+                                        _ => RESTResponse::with_text(404, "UTXO not found"),
+                                    }
+                                },
+                                Err(error) => RESTResponse::with_text(400, 
+                                    &format!("UTXO must be hex encoded vector of bytes: {error:?}") // TODO real format
+                                    .to_string()),
+                            },
+                            None => RESTResponse::with_text(400, "UTXO id must be provided"),
+                        }
+                    },
+                    _ => {
+                        error!("Unexpected message type {:?}", message);
+                        RESTResponse::with_text(500, "Unexpected message in REST request")
+                    }
+                };
+
+                Arc::new(Message::RESTResponse(response))
+            }
+        })?;
+
 
         Ok(())
     }
