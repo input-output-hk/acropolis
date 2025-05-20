570c02672d29c9b965f45af06c5055b1b4ccfa3b
Dmitry Shtukenberg <dmitry.shtukenberg@gmail.com>
2025-05-20 19:13:25 +0300

    Governance tracking changed to parsed files, file sizes reduced

--- processes/replayer/rename-governance.py
+++ processes/replayer/rename-governance.py
@@ -1,4 +1,21 @@
-json_files = [f for f in glob.glob('governance-dir/00*.json')]
+import glob
+import json
 
-for j in json_files:
-    print(j)
+json_files = [f for f in glob.glob('governance-logs/gov_00*.json')]
+
+idx = 0
+for j in sorted(json_files):
+    f = open(j)
+    data = json.load(f)
+    #data["ReceivedTxs"]["sequence"]["number"] = idx
+    #if idx > 1:
+    #    data["ReceivedTxs"]["sequence"]["previous"] = idx-1
+    #else:
+    #    data["ReceivedTxs"]["sequence"]["previous"] = None
+    f.close()
+
+    f = open('governance-logs/%d.json' % idx, 'wt')
+    json.dump(data, f)
+    f.close()
+
+    idx += 1
