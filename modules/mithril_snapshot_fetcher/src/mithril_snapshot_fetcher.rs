40efa18cefce1805bfcf0773320074becb7e81ca
Paul Clark <paul.clark@iohk.io>
2025-06-05 15:38:10 +0100

    Remove redundant QoS flag on Mithril publish
    Caryatid should now be free to delete it

--- modules/mithril_snapshot_fetcher/src/mithril_snapshot_fetcher.rs
+++ modules/mithril_snapshot_fetcher/src/mithril_snapshot_fetcher.rs
@@ -1,7 +1,7 @@
 //! Acropolis Mithril snapshot fetcher module for Caryatid
 //! Fetches a snapshot from Mithril and replays all the blocks in it
 
-use caryatid_sdk::{Context, Module, module, MessageBusExt, message_bus::QoS};
+use caryatid_sdk::{Context, Module, module, MessageBusExt};
 use acropolis_common::{
     calculations::slot_to_epoch, messages::{
         BlockBodyMessage, BlockHeaderMessage, Message, CardanoMessage,
@@ -250,8 +250,8 @@ impl MithrilSnapshotFetcher
                         block_info.clone(),
                         CardanoMessage::BlockHeader(header_message)
                     ));
-                    let header_future = context.message_bus.publish_with_qos(&header_topic,
-                        Arc::new(header_message_enum), QoS::Bulk);
+                    let header_future = context.message_bus.publish(&header_topic,
+                        Arc::new(header_message_enum));
 
                     // Send the block body message
                     let body_message = BlockBodyMessage {
@@ -262,8 +262,8 @@ impl MithrilSnapshotFetcher
                         block_info.clone(),
                         CardanoMessage::BlockBody(body_message)
                     ));
-                    let body_future = context.message_bus.publish_with_qos(&body_topic,
-                        Arc::new(body_message_enum), QoS::Bulk);
+                    let body_future = context.message_bus.publish(&body_topic,
+                        Arc::new(body_message_enum));
 
                     let (header_result, body_result) = join!(header_future, body_future);
                     header_result.unwrap_or_else(|e| error!("Failed to publish header: {e}"));
