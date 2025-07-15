80be7aa64f1b5613030ae4f87b4c357822e1a7a0
Paul Clark <paul.clark@iohk.io>
2025-07-15 18:26:00 +0100

    Remove 'sudo' in docker script (blush)

--- API/run-local-swagger.sh
+++ API/run-local-swagger.sh
@@ -5,4 +5,4 @@ set -e
 PORT=${1:-28080}
 HERE=$(realpath "$(dirname "${BASHSOURCE[0]}")")
 
-sudo docker run --name swagger --rm -p $PORT:8080 -e SWAGGER_JSON=/mount/openapi.yaml -v $HERE:/mount swaggerapi/swagger-ui
+docker run --name swagger --rm -p $PORT:8080 -e SWAGGER_JSON=/mount/openapi.yaml -v $HERE:/mount swaggerapi/swagger-ui
