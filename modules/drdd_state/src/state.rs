108122763345c137a32952e1d31b8f58847d9e66
William Hankins <william@sundae.fi>
2025-07-31 23:48:05 +0000

    feat: add drdd module with historical distribution REST endpoint
    Signed-off-by: William Hankins <william@sundae.fi>

---
+++ modules/drdd_state/src/state.rs
@@ -0,0 +1,53 @@
+use acropolis_common::DRepCredential;
+use std::collections::BTreeMap;
+use tracing::info;
+
+pub struct State {
+    historical_distributions: BTreeMap<u64, DRepDistribution>,
+}
+
+#[derive(Clone)]
+pub struct DRepDistribution {
+    pub dreps: BTreeMap<DRepCredential, u64>,
+    pub abstain: u64,
+    pub no_confidence: u64,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            historical_distributions: BTreeMap::new(),
+        }
+    }
+
+    pub fn insert_drdd(&mut self, epoch: u64, drdd: DRepDistribution) {
+        self.historical_distributions.insert(epoch, drdd);
+    }
+
+    pub fn get_latest(&self) -> Option<DRepDistribution> {
+        self.historical_distributions.last_key_value().map(|(_, map)| map.clone())
+    }
+
+    pub fn get_epoch(&self, epoch: u64) -> Option<DRepDistribution> {
+        self.historical_distributions.get(&epoch).cloned()
+    }
+
+    pub async fn tick(&self) -> anyhow::Result<()> {
+        let num_epochs = self.historical_distributions.len();
+        let latest = self.historical_distributions.iter().last();
+
+        if let Some((epoch, drep_map)) = latest {
+            let drep_count = drep_map.dreps.len();
+            info!(
+                num_epochs,
+                latest_epoch = *epoch,
+                drep_count,
+                "Tracking {num_epochs} epochs, latest is {epoch} with {drep_count} DReps"
+            );
+        } else {
+            info!("DRDD state: no data yet");
+        }
+
+        Ok(())
+    }
+}
