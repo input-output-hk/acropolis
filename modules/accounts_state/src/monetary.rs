b92278b830f9848d91ce113f5bc9f03ab132a79b
Paul Clark <paul.clark@iohk.io>
2025-08-20 13:16:14 +0100

    Fix _params in calculate_monetary_expansion (merge)

--- modules/accounts_state/src/monetary.rs
+++ modules/accounts_state/src/monetary.rs
@@ -90,7 +90,7 @@ fn calculate_eta(params: &ShelleyParams, total_non_obft_blocks: usize) -> Result
 }
 
 // Calculate monetary expansion based on current reserves
-fn calculate_monetary_expansion(_params: &ShelleyParams, reserves: Lovelace, eta: &BigDecimal)
+fn calculate_monetary_expansion(params: &ShelleyParams, reserves: Lovelace, eta: &BigDecimal)
                                 -> BigDecimal {
     let monetary_expansion_factor = params.protocol_params.monetary_expansion;
     let monetary_expansion = (BigDecimal::from(reserves)
