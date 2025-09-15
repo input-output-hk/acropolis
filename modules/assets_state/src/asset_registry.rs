94a5841c1f154c95c7ac894aef30bf75cc358663
William Hankins <william@sundae.fi>
2025-09-15 21:14:06 +0000

    test: add coverage for AssetRegistry
    Signed-off-by: William Hankins <william@sundae.fi>

--- modules/assets_state/src/asset_registry.rs
+++ modules/assets_state/src/asset_registry.rs
@@ -62,3 +62,90 @@ impl AssetRegistry {
         self.id_to_key.get(id.index())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::asset_registry::{AssetId, AssetRegistry};
+    use acropolis_common::{AssetName, PolicyId};
+
+    fn dummy_policy(byte: u8) -> PolicyId {
+        [byte; 28]
+    }
+
+    fn asset_name_from_str(s: &str) -> AssetName {
+        AssetName::new(s.as_bytes()).unwrap()
+    }
+
+    #[test]
+    fn only_insert_once() {
+        let mut registry = AssetRegistry::new();
+        let policy = dummy_policy(1);
+        let name = asset_name_from_str("tokenA");
+
+        let id1 = registry.get_or_insert(policy.clone(), name.clone());
+        let id2 = registry.get_or_insert(policy.clone(), name.clone());
+
+        assert_eq!(id1, id2);
+    }
+
+    #[test]
+    fn different_assets_get_different_ids() {
+        let mut registry = AssetRegistry::new();
+        let policy1 = dummy_policy(1);
+        let policy2 = dummy_policy(2);
+        let name1 = asset_name_from_str("tokenA");
+        let name2 = asset_name_from_str("tokenB");
+
+        let id1 = registry.get_or_insert(policy1.clone(), name1.clone());
+        let id2 = registry.get_or_insert(policy2.clone(), name2.clone());
+
+        assert_ne!(id1, id2);
+        assert_eq!(id1.index(), 0);
+        assert_eq!(id2.index(), 1);
+    }
+
+    #[test]
+    fn lookup_id_returns_correct_id() {
+        let mut registry = AssetRegistry::new();
+        let policy = dummy_policy(1);
+        let name = asset_name_from_str("tokenA");
+
+        let id1 = registry.get_or_insert(policy.clone(), name.clone());
+        let id2 = registry.lookup_id(&policy, &name).unwrap();
+
+        assert_eq!(id1, id2);
+    }
+
+    #[test]
+    fn lookup_id_returns_none_for_missing_key() {
+        let registry = AssetRegistry::new();
+        let policy = dummy_policy(9);
+        let name = asset_name_from_str("missing");
+
+        assert!(registry.lookup_id(&policy, &name).is_none());
+    }
+
+    #[test]
+    fn lookup_returns_correct_asset() {
+        let mut registry = AssetRegistry::new();
+        let policy = dummy_policy(1);
+        let name = asset_name_from_str("tokenA");
+
+        let id = registry.get_or_insert(policy.clone(), name.clone());
+        let key = registry.lookup(id).unwrap();
+
+        assert_eq!(policy, *key.policy);
+        assert_eq!(name, *key.name);
+    }
+
+    #[test]
+    fn lookup_returns_none_for_invalid_id() {
+        let mut registry = AssetRegistry::new();
+        let policy = dummy_policy(1);
+        let name = asset_name_from_str("tokenA");
+        registry.get_or_insert(policy, name);
+
+        let invalid_id = AssetId::new(999);
+        assert!(registry.lookup(invalid_id).is_none());
+    }
+}
