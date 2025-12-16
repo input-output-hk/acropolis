9fe574196953a618a6569562dd20408d6a43ef51
William Hankins <william@sundae.fi>
2025-12-16 00:01:23 +0000

    fix: allow configurable local port for swagger UI script
    Signed-off-by: William Hankins <william@sundae.fi>

--- API/run-local-swagger.sh
+++ API/run-local-swagger.sh
@@ -5,7 +5,7 @@ set -e
 PORT=${1:-28080}
 HERE=$(realpath "$(dirname "${BASHSOURCE[0]}")")
 
-docker run --name swagger --rm -p 28080:8080 \
-  -v $HERE/openapi.yaml:/usr/share/nginx/html/openapi.yaml \
-  -v $HERE/index.html:/usr/share/nginx/html/index.html \
+docker run --name swagger --rm -p "${PORT}:8080" \
+  -v "$HERE/openapi.yaml:/usr/share/nginx/html/openapi.yaml" \
+  -v "$HERE/index.html:/usr/share/nginx/html/index.html" \
   swaggerapi/swagger-ui
