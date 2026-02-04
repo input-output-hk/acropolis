c8a6f5d214188b4dd549f3ea1908ddc382cf78da
Krzysztof Paprocki <kris@anthouse.dev>
2026-02-04 08:05:21 +0100

    Rename

--- modules/drep_state/src/drep_state.rs
+++ modules/drep_state/src/drep_state.rs
@@ -164,7 +164,7 @@ impl DRepState {
         // and Shelly params (protocol version).
         let mut initial_d_rep_activity: Option<u32> = None;
         let mut initial_gov_action_lifetime: Option<u32> = None;
-        let mut is_bootstrap: Option<bool> = None;
+        let mut initial_is_bootstrap: Option<bool> = None;
 
         if let Some(params) = &mut subs.params {
             let (_, message) = params.read_skip_rollbacks().await?;
@@ -172,7 +172,7 @@ impl DRepState {
             // Snapshot may start mid-epoch, so read protocol params from genesis.
             if let (Some(shelley), Some(conway)) = (&message.params.shelley, &message.params.conway)
             {
-                is_bootstrap = Option::from(shelley.protocol_params.protocol_version.is_chang()?);
+                initial_is_bootstrap = Option::from(shelley.protocol_params.protocol_version.is_chang()?);
                 initial_d_rep_activity = Some(conway.d_rep_activity);
                 initial_gov_action_lifetime = Some(conway.gov_action_lifetime);
             } else if message.params.conway.is_some() {
@@ -191,7 +191,7 @@ impl DRepState {
                         storage_config,
                         initial_d_rep_activity,
                         initial_gov_action_lifetime,
-                        is_bootstrap,
+                        initial_is_bootstrap,
                     )
                 })
             };
