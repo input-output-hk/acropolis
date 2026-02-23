11f3bf47921933a582ee7f7cfc4b3e5bd41031ef
William Hankins <william@sundae.fi>
2026-02-23 19:38:20 +0000

    fix: use vendored protoc for CI support
    Signed-off-by: William Hankins <william@sundae.fi>

--- modules/midnight_state/build.rs
+++ modules/midnight_state/build.rs
@@ -1,6 +1,9 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path()?);
+
     tonic_build::configure()
         .build_server(true)
-        .compile_protos(&["proto/midnight_state.proto"], &["proto"])
-        .unwrap();
+        .compile_protos(&["proto/midnight_state.proto"], &["proto"])?;
+
+    Ok(())
 }
