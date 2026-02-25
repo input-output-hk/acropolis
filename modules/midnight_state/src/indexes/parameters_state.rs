18230514dbb9752536cf1587c98c501ca5c0b849
Will Hankins <william@sundae.fi>
2026-02-24 17:19:49 -0700

    refactor: use imbl structures for midnight_state (#748)
    Signed-off-by: William Hankins <william@sundae.fi>

--- modules/midnight_state/src/indexes/parameters_state.rs
+++ modules/midnight_state/src/indexes/parameters_state.rs
@@ -1,11 +1,10 @@
-use std::collections::BTreeMap;
-
 use acropolis_common::{Datum, Epoch};
+use imbl::OrdMap;
 
 #[derive(Clone, Default)]
 pub struct ParametersState {
     /// Ariadne parameters keyed by epoch
-    pub permissioned_candidates: BTreeMap<Epoch, Datum>,
+    pub permissioned_candidates: OrdMap<Epoch, Datum>,
 }
 
 impl ParametersState {
@@ -13,7 +12,7 @@ impl ParametersState {
     /// Insert the parameters for an epoch on change, overwriting existing entry if multiple
     /// updates in the same epoch
     pub fn add_parameter_datum(&mut self, epoch: Epoch, datum: Datum) -> bool {
-        if self.permissioned_candidates.last_key_value().map(|(_, v)| v) != Some(&datum) {
+        if self.permissioned_candidates.iter().next_back().map(|(_, v)| v) != Some(&datum) {
             self.permissioned_candidates.insert(epoch, datum);
             true
         } else {
