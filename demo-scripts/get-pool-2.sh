b337f83715105b27d214db580f98fdbc611ca55e
Krzysztof Paprocki <krzysztof.paprocki@iohk.io>
2026-04-24 20:02:51 +0200

    fix wrong testnet in getting script

--- demo-scripts/get-pool-2.sh
+++ demo-scripts/get-pool-2.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 echo "Pool 2 status (node 11):"
 
 docker run --rm \
--v "./testnet-forked/dbs/pool11:/ipc" \
+-v "./testnet/dbs/pool11:/ipc" \
 ghcr.io/intersectmbo/cardano-node:10.6.2 \
 cli query tip \
 --testnet-magic 42 \
